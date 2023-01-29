74b462ddc9ae6bf485bc31cc2c6a852e3157adaa
CodesBolt <123140179+CodesBolt@users.noreply.github.com>
2023-01-29 17:52:55 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -22,7 +22,7 @@ const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength, year) => {
   const yearStr = year === 'Total' ? '所有' : ` ${year} `;
-  return `我用 Strava 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据.有爱好不贵，坚持下来才贵，这就叫“贵在坚持”`;
+  return `我用 Strava 记录自己跑步 ${yearLength} 年了，下面列表展示的是${yearStr}的数据。有爱好不贵，坚持下来才贵，这就叫“贵在坚持”`;
 }
 const ENGLISH_INFO_MESSAGE = (yearLength, year) =>
   `Running Journey with ${yearLength} Years, the table shows year ${year} data`;
